c10b3ceaf19b76b47261e0e0559c532f3782cd68
sneq16 <72342172+ms16-c@users.noreply.github.com>
2026-06-07 03:22:27 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ import DiscordBot from "./bot";
 
 const bot = new DiscordBot();
 bot.start().catch(console.error);
+const http = require('http');
+http.createServer((req, res) => res.end('Bot is running!')).listen(process.env.PORT || 3000);
